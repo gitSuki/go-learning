5847abcf8463f5ab619eba12b68cabfd6ca486a1
unknown <collinbrown226@gmail.com>
2023-01-23 16:06:29 -0600

    Review some of the fundamentals of go

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
+// a package is a collection of common source code files. Every single file must declare the package that it belongs to
+// there are two types of packages. Executable (something we can run) and reusable (something used as helpers/libraries)
+// we named this package main because any package named main turns into an executable. a package with any other name will instead bo considered a reusable package, aka a helper library/dependency
 package main
 
+// fmt is a standard package included with golang by default. fmt is short for "format"
 import "fmt"
 
 func main() {
