05f49f757e9e57554a674c9bb9b54ced9e3d2d03
gitSuki <collinbrown226@gmail.com>
2023-01-25 07:09:31 -0600

    Add utils code for generating random values for tests

---
+++ simple-bank/util/random.go
@@ -0,0 +1,42 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RandomInt generates a random int between min and max
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+// RandomString generates a random string of length n
+func RandomString(n int) string {
+	var sb strings.Builder
+	alphabet := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	length := len(alphabet)
+	for i := 0; i < n; i++ {
+		char := alphabet[rand.Intn(length)]
+		sb.WriteByte(char)
+	}
+	return sb.String()
+}
+
+func RandomOwner() string {
+	return RandomString(8)
+}
+
+func RandomMoney() int {
+	return int(RandomInt(1, 10000))
+}
+
+func RandomCurrency() string {
+	currencies := []string{"USD", "CAD", "EUR"}
+	length := len(currencies)
+	return currencies[rand.Intn(length)]
+}
