5898685ecb92a940df822ae80f0bd81b67dc378e
gitSuki <collinbrown226@gmail.com>
2023-01-25 04:44:28 -0600

    Create get queries for account

--- simple-bank/db/query/account.sql
+++ simple-bank/db/query/account.sql
@@ -3,4 +3,15 @@ INSERT INTO accounts (
   owner, balance, currency
 ) VALUES (
   $1, $2, $3
-) RETURNING * ;
+) 
+RETURNING * ;
+
+-- name: GetAccount :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1;
+
+-- name: ListAccounts :many
+SELECT * FROM accounts
+ORDER BY id
+LIMIT $1
+OFFSET $2;
