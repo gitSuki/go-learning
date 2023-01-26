5a21f6ece796f549945fe3c9bf3e9de00b9db108
gitSuki <collinbrown226@gmail.com>
2023-01-26 06:32:24 -0600

    Create Store struct and execute transaction function

---
+++ simple-bank/db/sqlc/store.go
@@ -0,0 +1,40 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+// provides all functions to execute db queries individually as well as combined in a TX
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+// NewStore creates a new store
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
+
+// execTX executes a function within a database tx
+func (store *Store) execTX(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	queries := New(tx)
+	err = fn(queries)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx error: %v, rb err: %v", err, rbErr)
+		}
+		return err
+	}
+
+	return tx.Commit()
+}
