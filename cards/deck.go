2d63366ae97cf3ec6f18d4888b075cf156350a21
gitSuki <collinbrown226@gmail.com>
2023-01-25 18:50:51 -0600

    Create toString() method

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // create a new type of deck
 // a slice of strings.
@@ -33,4 +36,8 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ", ")
 }
--- cards/main.go
+++ cards/main.go
@@ -1,12 +1,13 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	// Arrays are always fixed length, a Slice can grow or shrink.
 	// Every element in the slice must be of the same data type
 	// the data type is inferred when using :=
 	cards := newDeck()
-	cards.print()
-	hand, remainingCards := deal(cards, 5)
-	hand.print()
-	remainingCards.print()
+	fmt.Println(cards.toString())
 }
