2780aab41b76c5cc4b5784c4a7a0a15e572343a3
gitSuki <collinbrown226@gmail.com>
2023-01-27 16:18:06 -0600

    Implement main function for running db and server

---
+++ simple-bank/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/gitsuki/simplebank/api"
+	db "github.com/gitsuki/simplebank/db/sqlc"
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8000"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("Cannot connect to database")
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("Cannot start server", err)
+	}
+}
