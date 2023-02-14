4ae3c9f65d03f284ed3aa2883e815def4f8c7a9e
gitSuki <collinbrown226@gmail.com>
2023-02-14 17:28:41 -0600

    Add a function for creating a new token payload

--- simple-bank/token/payload.go
+++ simple-bank/token/payload.go
@@ -13,3 +13,19 @@ type Payload struct {
 	IssuedAt  time.Time `json:"issued_at"`
 	ExpiredAt time.Time `kson:"expired_at"`
 }
+
+// NewPayload creates a new token payload with a specific username and duration
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	payload := &Payload{
+		ID:        tokenID,
+		Username:  username,
+		IssuedAt:  time.Now(),
+		ExpiredAt: time.Now().Add(duration),
+	}
+	return payload, nil
+}
