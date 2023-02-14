570bc9f710dcdc7b94f6f80f7e9442ca5f24e8b5
gitSuki <collinbrown226@gmail.com>
2023-02-14 16:37:10 -0600

    Add uti tests to password hashing util functions

--- simple-bank/util/password.go
+++ simple-bank/util/password.go
@@ -10,12 +10,12 @@ import (
 func HashPassword(password string) (string, error) {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		return "", fmt.Errorf("failed to hash password: %s", err)
+		return "", fmt.Errorf("failed to hash password: %w", err)
 	}
 	return string(hashedPassword), nil
 }
 
-// CheckPassword checks if the provided password is the same as the provided hashed password
+// CheckPassword checks if the provided password is correct or not
 func CheckPassword(password string, hashedPassword string) error {
-	return bcrypt.CompareHashAndPassword([]byte(password), []byte(hashedPassword))
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
 }
