99efae738bea510d05926e2ba0e0a4533966d82b
gitSuki <collinbrown226@gmail.com>
2023-02-14 17:34:42 -0600

    Add boilerplate for a json web token maker

---
+++ simple-bank/token/jwt_maker.go
@@ -0,0 +1,27 @@
+package token
+
+import (
+	"fmt"
+	"time"
+)
+
+const minSecretKeySize = 32
+
+// JWTMaker is a JSON Web Token maker
+type JWTMaker struct {
+	secretKey string
+}
+
+// NewJTMaker creates a new JWTMaker
+func NewJWTMaker(secretKey string) (Maker, error) {
+	if len(secretKey) <= minSecretKeySize {
+		return nil, fmt.Errorf("invalid key size: must be at least %d characters", minSecretKeySize)
+	}
+	return &JWTMaker{secretKey}, nil
+}
+
+// CreateToken creates a new token for a specific username and duration
+func (maker *JWTMaker) CreateToken(username string, duration time.Duration) (string, error)
+
+// VerifyToken checks if the token is valid or not
+func (maker *JWTMaker) VerifyToken(token string) (*Payload, error)
