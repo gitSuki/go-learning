9fe40ccd7f9e075b7652edce054e150cc1d082f8
gitSuki <collinbrown226@gmail.com>
2023-02-15 11:10:16 -0600

    Implement create and verify token methods for paseto

--- simple-bank/token/paseto_maker.go
+++ simple-bank/token/paseto_maker.go
@@ -2,6 +2,7 @@ package token
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/aead/chacha20poly1305"
 	"github.com/o1egl/paseto"
@@ -23,4 +24,31 @@ func NewPasetoMaker(symmetricKey string) (Maker, error) {
 		symmetricKey: []byte(symmetricKey),
 	}
 	return maker, nil
+}
+
+// CreateToken creates a new token for a specific username and duration
+func (maker *PasetoMaker) CreateToken(username string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", err
+	}
+
+	return maker.paseto.Encrypt(maker.symmetricKey, payload, nil)
+}
+
+// VerifyToken checks if the token is valid or not
+func (maker *PasetoMaker) VerifyToken(token string) (*Payload, error) {
+	payload := &Payload{}
+
+	err := maker.paseto.Decrypt(token, maker.symmetricKey, payload, nil)
+	if err != nil {
+		return nil, ErrInvalidToken
+	}
+
+	err = payload.Valid()
+	if err != nil {
+		return nil, err
+	}
+
+	return payload, nil
 }
