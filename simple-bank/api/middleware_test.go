2b76f0cd2f914279083e328ab139ac5be6401f9b
gitSuki <collinbrown226@gmail.com>
2023-02-16 12:40:29 -0600

    Add test case for using an unsupported authorizationrization format

--- simple-bank/api/middleware_test.go
+++ simple-bank/api/middleware_test.go
@@ -58,6 +58,15 @@ func TestAuthMiddleware(t *testing.T) {
 				require.Equal(t, http.StatusUnauthorized, recorder.Code)
 			},
 		},
+		{
+			name: "InvalidAuthorizationFormat",
+			setupAuth: func(t *testing.T, request *http.Request, tokenMaker token.Maker) {
+				addAuthorization(t, request, tokenMaker, "", "user", time.Minute)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusUnauthorized, recorder.Code)
+			},
+		},
 	}
 
 	for i := range testCases {
