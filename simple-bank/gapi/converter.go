9b3665b6517442b4ffbd70c9cd148b1ab7383b55
gitSuki <collinbrown226@gmail.com>
2023-02-19 14:35:40 -0600

    Fix bug with convertUser method not converting email

--- simple-bank/gapi/converter.go
+++ simple-bank/gapi/converter.go
@@ -10,6 +10,7 @@ func convertUser(user db.User) *pb.User {
 	return &pb.User{
 		Username:          user.Username,
 		FullName:          user.FullName,
+		Email:             user.Email,
 		PasswordChangedAt: timestamppb.New(user.PasswordChangedAt),
 		CreatedAt:         timestamppb.New(user.CreatedAt),
 	}
