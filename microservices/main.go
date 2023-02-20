ab6ef2ab41c025e2429357b63e6a1a13febdcadb
gitSuki <collinbrown226@gmail.com>
2023-02-19 18:56:22 -0600

    Complete basic  http response using default http package

--- microservices/main.go
+++ microservices/main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		log.Println("Hello world")
+		d, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "Unable to read!", http.StatusBadRequest)
+			return
+		}
+
+		log.Println(d)
+		fmt.Fprintf(rw, "Hello %s", d)
+	})
+
+	http.HandleFunc("/goodbye", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Goodbye world")
 	})
 	http.ListenAndServe(":8000", nil)
 }
