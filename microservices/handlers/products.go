5e08d8920155556d3e049eb33f6e32cdbbb711af
gitSuki <collinbrown226@gmail.com>
2023-02-19 20:04:38 -0600

    Modularize products handler to split get status method into seperate function

--- microservices/handlers/products.go
+++ microservices/handlers/products.go
@@ -15,7 +15,17 @@ func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
-func (p *Products) ServeHTTP(rw http.ResponseWriter, h *http.Request) {
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return
+	}
+
+	// catch all
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	productsList := data.GetProducts()
 	err := productsList.ToJSON(rw)
 	if err != nil {
