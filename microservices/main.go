a26b5a61f2a0bf5e50b0760408758eb7a6a9b6c0
gitSuki <collinbrown226@gmail.com>
2023-02-19 19:24:19 -0600

    Add custom paramters for server

--- microservices/main.go
+++ microservices/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gitsuki/microservices/handlers"
 )
@@ -17,5 +18,12 @@ func main() {
 	serveMux.Handle("/", helloHandler)
 	serveMux.Handle("/goodbye", goodbyeHandler)
 
-	http.ListenAndServe(":8000", serveMux)
+	server := &http.Server{
+		Addr:         ":8000",
+		Handler:      serveMux,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+	server.ListenAndServe()
 }
