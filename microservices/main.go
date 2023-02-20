15c79e88d42e925d47102dccacf7b34cc1f6c8d9
gitSuki <collinbrown226@gmail.com>
2023-02-19 19:34:09 -0600

    Implement graceful shutdown for server

--- microservices/main.go
+++ microservices/main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
 
 	"github.com/gitsuki/microservices/handlers"
 )
 
 func main() {
-	log := log.New(os.Stdout, "product-api", log.LstdFlags)
-	helloHandler := handlers.NewHello(log)
-	goodbyeHandler := handlers.NewGoodbye(log)
+	customLog := log.New(os.Stdout, "product-api", log.LstdFlags)
+	helloHandler := handlers.NewHello(customLog)
+	goodbyeHandler := handlers.NewGoodbye(customLog)
 
 	serveMux := http.NewServeMux()
 	serveMux.Handle("/", helloHandler)
@@ -25,5 +27,21 @@ func main() {
 		ReadTimeout:  1 * time.Second,
 		WriteTimeout: 1 * time.Second,
 	}
-	server.ListenAndServe()
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			customLog.Fatal(err)
+		}
+	}()
+
+	signalChannel := make(chan os.Signal)
+	signal.Notify(signalChannel, os.Interrupt)
+	signal.Notify(signalChannel, os.Kill)
+
+	sig := <-signalChannel
+	customLog.Println("Recieved terminate, graceful shutdown", sig)
+
+	timeoutCTX, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	server.Shutdown(timeoutCTX)
 }
