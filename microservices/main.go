c31ad02be0f99461eefc629d240ac663b6b0d935
gitSuki <collinbrown226@gmail.com>
2023-02-19 19:19:40 -0600

    Create handler for goodbye

--- microservices/main.go
+++ microservices/main.go
@@ -11,9 +11,11 @@ import (
 func main() {
 	log := log.New(os.Stdout, "product-api", log.LstdFlags)
 	helloHandler := handlers.NewHello(log)
+	goodbyeHandler := handlers.NewGoodbye(log)
 
 	serveMux := http.NewServeMux()
 	serveMux.Handle("/", helloHandler)
+	serveMux.Handle("/goodbye", goodbyeHandler)
 
 	http.ListenAndServe(":8000", serveMux)
 }
